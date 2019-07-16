4cbec5523a20e6d14f27a4fdb747dfb9647e4b84
Blueshadow58 <gamonalfranco@gmail.com>
2019-07-16 09:20:39 -0400

    background y css

--- src/Components/PasajeroLista.js
+++ src/Components/PasajeroLista.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Axios from "axios";
-import { table,tbody,thead,tr,th,td } from "react-foundation";
+import { Grid, Cell } from "react-foundation";
+import "./compo.css";
 
 export default class PasajeroLista extends React.Component {
   state = {
@@ -16,31 +17,37 @@ export default class PasajeroLista extends React.Component {
 
   render() {
     return (
-     
-     
 
-      <table color={Colors.WARNING}>
-        <thead >
-          <tr>
-            <th className="text-center">Id</th>
-            <th className="text-center">Nombre</th>
-            <th className="text-center">Apellido</th>
-            <th className="text-center">Nacionalidad</th>
-          </tr>
-        </thead>
-        <tbody>
-                    
-            {this.state.pasajeros.map(pasajero => {
-              return <tr key={pasajero.id}>
-            <td>{pasajero.id}</td>
-            <td>{pasajero.nombre}</td>
-            <td>{pasajero.apellido}</td>
-            <td>{pasajero.nacionalidad}</td>
-            </tr>
-            })}          
-        </tbody>
-      </table>
-    
+      <div className="background">
+      <Grid className="">
+        <Cell small={3} />
+        <Cell small={6} className="jump">
+          <table id="custom">
+            <thead>
+              <tr>
+                <th >Id</th>
+                <th >Nombre</th>
+                <th >Apellido</th>
+                <th >Nacionalidad</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.pasajeros.map(pasajero => {
+                return (
+                  <tr key={pasajero.id}>
+                    <td>{pasajero.id}</td>
+                    <td>{pasajero.nombre}</td>
+                    <td>{pasajero.apellido}</td>
+                    <td>{pasajero.nacionalidad}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </Cell>
+        <Cell small={3} />
+      </Grid>
+      </div>
 
 
     );
